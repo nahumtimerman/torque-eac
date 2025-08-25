6c6ad87e8ad9ec19b6677922644797c25baa493a
nahumtimerman <ntimerman@gmail.com>
2025-08-25 14:56:56 +0300

    Create variables.tf

---
+++ terraform/simple-tf-module/variables.tf
@@ -0,0 +1,19 @@
+variable "test_input1" {
+    type = string
+    default = "test input 1 value"
+}
+
+variable "test_input2" {
+    type = string
+}
+
+variable "test_input3" {
+    type = list(string)
+    default = []
+}
+
+variable "test_input4" {
+    type = string
+    description = "test input4 description"
+    default = ""
+}
