e3417c2d1f307cb61f77a08fe67accfb933ad2bd
nahumtimerman <ntimerman@gmail.com>
2025-08-28 10:51:04 +0300

    Update main.tf

--- terraform/buggy/main.tf
+++ terraform/buggy/main.tf
@@ -1,1 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "broken" {
+  # Invalid argument: `bucket` name cannot contain uppercase letters
+  bucket = "MyInvalidBucketName"
+}
 
